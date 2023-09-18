750ddf9d6f18a12bec24aada4703d58577c2f27e
Sahasrajit Sarmasarkar <sahasrajitsarmasarkar@Sahasrajits-MacBook-Air-3.local>
2023-09-18 12:42:10 +0530

    Openstreetmap-working-without-var-cost-projects

--- public/js/gmap.js
+++ public/js/gmap.js
@@ -1,19 +1,29 @@
+function extendObj(obj1, obj2){
+    for (var key in obj2){
+        if(obj2.hasOwnProperty(key)){
+            obj1[key] = obj2[key];
+        }
+    }
+
+    return obj1;
+}
 // a wrapper object for a Google Maps object in the map
 function GMapGeometry(map, title, g, opts) {
   var c = g.coordinates;
   if (typeof opts === 'undefined')
     opts = {};
 
+  /*
   if (g.type == 'Point') {
-    this.gmapObject = new google.maps.Marker($.extend({
-      position: new google.maps.LatLng(c[0], c[1]),
+    this.gmapObject = new L.marker($.extend({
+      position: new L.latLng(c[0], c[1]),
       map: map
     }, opts));
   } else if (g.type == 'LineString') {
     var path = [];
     for (var i = 0; i < c.length; ++i)
-      path.push( new google.maps.LatLng(c[i][0], c[i][1]) );
-    this.gmapObject = new google.maps.Polyline($.extend({
+      path.push( new L.latLng(c[i][0], c[i][1]) );
+    this.gmapObject = new L.polyline($.extend({
       path: path,
       strokeColor: '#428bca',
       strokeOpacity: 1,
@@ -25,8 +35,8 @@ function GMapGeometry(map, title, g, opts) {
     var r = c;
     var path = [];
     for (var i = 0; i < r.length; ++i)
-      path.push( new google.maps.LatLng(r[i][0], r[i][1]) );
-    this.gmapObject = new google.maps.Polygon($.extend({
+      path.push( new L.latLng(r[i][0], r[i][1]) );
+    this.gmapObject = new L.polygon($.extend({
       path: path,
       strokeColor: '#428bca',
       strokeOpacity: 1,
@@ -34,16 +44,37 @@ function GMapGeometry(map, title, g, opts) {
       map: map
     }, opts));
   }
+  */
+
+  if (g.type == 'Point') {
+    this.gmapObject = L.marker(new L.latLng(c[0], c[1]),opts).addTo(map);
+  } else if (g.type == 'LineString') {
+    var path = [];
+    for (var i = 0; i < c.length; ++i)
+      path.push( new L.latLng(c[i][0], c[i][1]) );
+    this.gmapObject = L.polyline(path,extendObj({color:'#428bca',weight:6,opacity:1},opts)).addTo(map);
+  } else if (g.type == 'Polygon') {
+    //var r = c[0]; // the exterior ring
+    var r = c;
+    var path = [];
+    for (var i = 0; i < r.length; ++i)
+      path.push( new L.latLng(r[i][0], r[i][1]) );
+    this.gmapObject = L.polygon(path,extendObj({color:'#428bca',weight:2,opacity:1},opts)).addTo(map);
+      
+  }
+
+
+
 
   this.center = function() {
     if (g.type == 'Point') {
-      return new google.maps.LatLng(c[0], c[1]);
+      return new L.latLng(c[0], c[1]);
     } else if (g.type == 'LineString') {
-      return new google.maps.LatLng(c[c.length-1][0], (c[0][1]+c[1][1])/2);
+      return new L.latLng(c[c.length-1][0], (c[0][1]+c[1][1])/2);
     } else if (g.type == 'Polygon') {
-      var bounds = new google.maps.LatLngBounds();
+      var bounds = new L.latLngBounds();
       for (var i = 0; i < c.length; i++) {
-        bounds.extend(new google.maps.LatLng(c[i][0], c[i][1]));
+        bounds.extend(new L.latLng(c[i][0], c[i][1]));
       }
       return bounds.getCenter();
     }
@@ -66,8 +97,10 @@ function GMap(id, info_id, data, zoom, templates, mapOpts) {
   var currentCost = 0;
 
   function initialize() {
+
+    /*
     var mapOptions = $.extend({
-      center: new google.maps.LatLng(data.ward_center[0], data.ward_center[1]),
+      center: new L.maps.LatLng(data.ward_center[0], data.ward_center[1]),
       minZoom: 6,
       zoom: zoom ? zoom : 14,
       panControl: false,
@@ -76,8 +109,11 @@ function GMap(id, info_id, data, zoom, templates, mapOpts) {
       scrollwheel: false,
       fullscreenControl: false
     }, mapOpts);
+    */
 
-    var map = new google.maps.Map(document.getElementById(id), mapOptions);
+    
+
+    var map = new L.map(document.getElementById(id), mapOptions);
     obj.map = map;
 
     var styles = [
@@ -101,7 +137,22 @@ function GMap(id, info_id, data, zoom, templates, mapOpts) {
         ]
       }
     ];
-    map.setOptions({styles: styles});
+
+    var mapOptions = extendObj({
+      center: new L.maps.LatLng(data.ward_center[0], data.ward_center[1]),
+      minZoom: 6,
+      zoom: zoom ? zoom : 14,
+      panControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
+      scrollwheel: false,
+      fullscreenControl: false,
+      styles: styles,
+    }, mapOpts);
+
+    var map = new L.map(document.getElementById(id), mapOptions);
+    obj.map = map;
+    //map.setOptions({styles: styles});
 
     var highlightedStreet = null;
     var highlightStreet = function (s) {
@@ -284,20 +335,13 @@ function GMap(id, info_id, data, zoom, templates, mapOpts) {
 // Embed a Google Map given the id and location of the project
 function drawMap(id, objects) {
   // Use some default lattitude and longitude
-  var map_center = new google.maps.LatLng(41.8337329, -87.7321555);
+  var map_center = new L.latLng(41.8337329, -87.7321555);
+
+
 
   // Declare the map options
   var mapCanvas = document.getElementById(id);
-  var mapOptions = {
-    center: map_center,
-    zoom: 14,
-    panControl: false,
-    streetViewControl: false,
-    mapTypeControl: false,
-    scrollwheel: false,
-    fullscreenControl: false
-  }
-
+  
   // Declare the Google Map style
   var mapStyles = [
     {
@@ -309,33 +353,75 @@ function drawMap(id, objects) {
     }
   ];
 
+  var mapOptions = {
+    //center: map_center,
+    zoom: 14,
+    panControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    scrollwheel: false,
+    fullscreenControl: false,
+    featureType: "road",
+    elementType: "geometry",
+      stylers: [
+        { visibility: "simplified" }
+      ]
+    //styles: mapStyles,
+  };
+
+
   // Declare a Google map
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  map.setOptions({styles: mapStyles});
+  var map = new L.map(mapCanvas, mapOptions);
+
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      //styles:mapStyles,
+      //maxZoom: 18,
+    }).addTo(map);
+
+
+  //L.setOptions(map,{styles: mapStyles});
 
   // Reduce the zoom
   if (objects.length > 1)
     map.setZoom(13);
 
-  var markerBounds = new google.maps.LatLngBounds();
+  var markerBounds = new L.latLngBounds();
+
+  
 
   // Plot markers at all the locations
   for (var i = 0; i < objects.length; i++) {
     var object = objects[i];
     if ($.isArray(object)) {
       var coordinates = object;
-      var markerLocation = new google.maps.LatLng(coordinates[0], coordinates[1]);
-      new google.maps.Marker({
-        map: map,
-        position: markerLocation
-      });
+      var markerLocation = new L.latLng(coordinates[0], coordinates[1]);
+      //new L.marker({
+      //  map: map,
+      //  position: markerLocation
+      //});
+      marker = L.marker(markerLocation).addTo(map);
+      
       markerBounds.extend(markerLocation);
     } else {
       var o = new GMapGeometry(map, null, object, {});
+
+      console.log(o);
+      
       markerBounds.extend(o.center());
     }
   }
 
+  //console.log(points);
+
+  //var markerBounds = new L.latLngBounds(points); 
+  //console.log(markerBounds.getCenter());
+
   // Center the map at the center of the locations
-  map.setCenter(markerBounds.getCenter());
+  //map = L.setCenter(map,markerBounds.getCenter());
+  //map.setCenter(markerBounds.getCenter())
+  //marker = L.marker(markerBounds.getCenter()).addTo(map);
+  map.panTo(markerBounds.getCenter());
+
+  //map.setView(markerBounds.getCenter(), 25);
 }
